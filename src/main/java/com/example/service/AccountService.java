bf0f5642326310f3ea2070f35fb4ec8dc8ebfb8d
Derek Williams <dwilliams33dw@gmail.com>
2025-05-12 21:47:09 +0000

    updated account service and repository

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,23 @@
 package com.example.service;
 
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
+@Transactional
 public class AccountService {
+
+    AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public Account addAccount(Account account) {
+        return accountRepository.save(account);
+    }
 }
