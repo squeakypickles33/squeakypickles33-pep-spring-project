39cdba426a58b74b5386e61387d1db9cb77fd248
Derek Williams <dwilliams33dw@gmail.com>
2025-05-12 22:19:33 +0000

    Updated message service and repository

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,23 @@
 package com.example.service;
 
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.entity.Message;
+import com.example.repository.MessageRepository;
+
+@Service
+@Transactional
 public class MessageService {
+
+    MessageRepository messageRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public Message addMessage(Message message) {
+        return messageRepository.save(message);
+    }
 }
