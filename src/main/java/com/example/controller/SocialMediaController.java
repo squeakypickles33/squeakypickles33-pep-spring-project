b68b7f50e1d48b3eca5b6fa6b540dc1814c3f399
Derek Williams <dwilliams33dw@gmail.com>
2025-05-13 16:59:43 +0000

    added register and login controllers

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,8 +1,15 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.service.AccountService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -13,4 +20,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SocialMediaController {
 
+    @Autowired
+    private AccountService accountService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Account> register(@RequestBody Account account) {
+        try {
+            Account newAccount = accountService.registerAccount(account);
+            return new ResponseEntity<>(newAccount, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.CONFLICT);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Account> login(@RequestParam String username, @RequestParam String password) {
+        try {
+            Account account = accountService.loginAccount(username, password);
+            return new ResponseEntity<>(account, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+        }
+    }
+    
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,5 +1,6 @@
 package com.example.service;
 
+import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,24 @@ public class AccountService {
 
     public Account addAccount(Account account) {
         return accountRepository.save(account);
+    }
+
+    public Account registerAccount(Account account) throws Exception {
+        if (account.getUsername().isBlank() || account.getPassword().length() < 4) {
+            throw new IllegalArgumentException("Invalid username or password is too short.");
+        }
+        if (accountRepository.findByUsername(account.getUsername()).isPresent()) {
+            throw new Exception("Username already exists.");
+        }
+        return accountRepository.save(account);
+    }
+
+    public Account loginAccount(String username, String password) throws Exception {
+        Optional<Account> account = accountRepository.findByUsername(username);
+        if (account.isPresent() && account.get().getPassword().equals(password)) {
+            return account.get();
+        } else {
+            throw new Exception("Invalid password or username.");
+        }
     }
 }
